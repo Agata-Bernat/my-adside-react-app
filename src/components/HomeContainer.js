fd1cdaf23ae0e2720cfe569833342eebadb64014
Agata-Bernat <agata.bernat.art@gmail.com>
2021-02-25 13:45:52 +0100

    just starting

---
+++ src/components/HomeContainer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function HomeContainer() {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li>About</li>
+                    <li>Featured</li>
+                    <li>Portfolio</li>
+                    <li>Blog</li>
+                    <li>Contact</li>
+
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default HomeContainer;
+
