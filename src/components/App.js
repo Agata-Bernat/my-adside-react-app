1ff50d8f129639bbbc2a67dfc366ad933183ab84
Agata-Bernat <agata.bernat.art@gmail.com>
2021-04-15 16:11:24 +0200

    created and use DataBase with one of the pages-Blog

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import HomeContainer from './HomeContainer';
 import About from './About';
 import Featured from './Featured';
@@ -7,7 +7,12 @@ import Blog from './Blog';
 import Contact from './Contact';
 import { Route } from 'react-router-dom';
 
+
 function App() {
+  const [pages, setPages] = useState([])
+  useEffect(() => {
+    fetch('http://localhost:3000/pages').then(response => response.json()).then(data => setPages(data))
+  }, []); 
   return (
     <div className="App">
       <HomeContainer />
@@ -21,7 +26,7 @@ function App() {
       <Portfolio />
       </Route>
       <Route path = "/Blog">
-      <Blog />
+      <Blog content = {pages[0]}/>
       </Route>
       <Route path = "/Contact">
       <Contact />
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,24 +2,38 @@ import React, {useState, useEffect} from 'react';
 
 function Portfolio() {
 
-    const [data, setData] = useState(null);
-    const access_key = "rvmwo2cCJTt8CoiNAWOc-j5OgX52gGpaq6A4E43KwtI";
-    const APIlink = `https://api.unsplash.com/photos/random?client_id=${access_key}`;
+    const [data, setData] = useState();
+    //const access_key = "rvmwo2cCJTt8CoiNAWOc-j5OgX52gGpaq6A4E43KwtI";
+    //const APIlink = `https://api.unsplash.com/photos/random?client_id=${access_key}`;
+    const APIlink = "https://api.themoviedb.org/3/discover/movie?api_key=14df7912217b3871d3af70261869c1c6";
     
     useEffect(async () => {
         const response = await fetch(APIlink);
         const resData = await response.json();
         console.log(resData);
-        setData(resData);
+        setData(resData.results);
     }, []);
 
     return (
-        <div>
-            {/*here will gone be bootstrap gred cards part. Need to find how we done that before. */}
-            <p>my last masterpice</p> {data && <img src={data.urls.regular} />}
+   
+      <div className="movie-cards" >
+      {data.map((movie) => (
+        <div className="movie" key={movie.id}>
+        <img
+        src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
+        />
+        <h3>Title: {movie.original_title}</h3>
+        <p>Overview: {movie.overview}</p>
+        <p>Popularity: {movie.popularity}</p>
+        <p>Release Date: {movie.release_date}</p>
+        <p>Video:{movie.video}</p>
+        <p>Vote-Average: {movie.vote_average}</p>
         </div>
-    )
-}
+      
+      ))}
+      </div>
+    )}
+
 
 export default Portfolio;
 
